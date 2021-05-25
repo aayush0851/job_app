b849e7dbc5045b97e61dc627164204c631b724ba
Aayush Tyagi <tyagiayush851@gmail.com>
2021-05-26 01:36:06 +0530

    port fix

--- src/utils/handleEnv.ts
+++ src/utils/handleEnv.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 const ENVPATH = ".env";
 dotenv.config({path: ENVPATH});
 
-export const ENV_APP_PORT = +process.env.APP_PORT;
+export const ENV_APP_PORT = +process.env.PORT;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const MONGO_URL = process.env.MONGO_URL;
 export const HOST_EMAIL = process.env.NODEMAILER_HOST_EMAIL;
